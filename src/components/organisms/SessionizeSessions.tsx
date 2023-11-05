5f30817a7af4e168af94f8d96ff292b1f4f04db2
Hal Seki <hal@plants-web.jp>
2023-11-05 14:41:07 +0900

    select date and set the index as the hash url

--- src/components/organisms/SessionizeSessions.tsx
+++ src/components/organisms/SessionizeSessions.tsx
@@ -1,19 +1,29 @@
 "use client";
 import useSessionizeGrids from "@/hooks/useSessionizeData";
-import React from "react";
+import React, { useEffect } from "react";
 import { format } from "date-fns";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 // props of session ID
 type Props = {
   id: string;
 };
 
 const SessionizeSessions: React.FC<Props> = ({ id }) => {
+  const router = useRouter();
+  const [hash, setHash] = React.useState("1");
   // get hash from URL
-  const hash = window.location.hash
-    ? window.location.hash.replace("#", "")
-    : "1";
-  console.log(hash);
+  useEffect(() => {
+    const _hash = window.location.hash
+      ? window.location.hash.replace("#", "")
+      : "1";
+    setHash(_hash);
+  }, []);
+  // change hash and URL
+  const changeDate = (index: number) => () => {
+    setHash(index.toString());
+    router.push(`/#${index.toString()}`);
+  };
   const { grids, isLoading, error } = useSessionizeGrids(id);
   return (
     <div className="schedule">
@@ -25,14 +35,16 @@ const SessionizeSessions: React.FC<Props> = ({ id }) => {
             {grids.map((grid, index) => (
               <div key={index}>
                 {index + 1 !== Number(hash) ? (
-                  <Link
-                    href={{ hash: (index + 1).toString() }}
+                  <button
+                    onClick={changeDate(index + 1)}
                     className="text-white bg-primary hover:bg-secondary focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                   >
                     {format(grid.date, "M月d日")}
-                  </Link>
+                  </button>
                 ) : (
-                  <div>{format(grid.date, "M月d日")}</div>
+                  <div className="text-primary focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    {format(grid.date, "M月d日")}
+                  </div>
                 )}
               </div>
             ))}
