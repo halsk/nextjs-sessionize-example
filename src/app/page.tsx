7e6e9e18f68e57bac4259986f18ccb7bbf877466
Hal Seki <hal@plants-web.jp>
2023-11-11 22:36:37 +0900

    use .env file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SessionizeSessions id="xbkmhusk" />
+      {process.env.NEXT_PUBLIC_SESSIONIZE_ID ? (
+        <SessionizeSessions id={process.env.NEXT_PUBLIC_SESSIONIZE_ID!} />
+      ) : (
+        <div className="flex flex-col items-center justify-center">
+          Please create .env.local and set NEXT_PUBLIC_SESSIONIZE_ID{" "}
+        </div>
+      )}
     </main>
   );
 }
