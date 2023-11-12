b5f160b157a6d07ec8a3cc98efb34a0db5972345
Hal Seki <hal@plants-web.jp>
2023-11-12 23:23:07 +0900

    remove unused class

--- src/components/atoms/SessionDetail.tsx
+++ src/components/atoms/SessionDetail.tsx
@@ -19,7 +19,7 @@ export const SessionDetail: React.FC<Props> = ({
   return (
     <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
       <div className="sm:flex sm:items-start">
-        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+        <div className="mt-3 sm:ml-4 sm:mt-0 sm:text-left">
           <Dialog.Title
             as="h3"
             className="text-lg font-semibold leading-6 text-gray-900 text-left"
