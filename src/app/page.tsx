9f147207020b98da7aa55fb7535eed096b660d5d
Hal Seki <hal@plants-web.jp>
2023-11-29 08:47:50 +0900

    add event logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 import SessionizeSessions from "@/components/organisms/SessionizeSessions";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="container mx-auto">
+      <Link href="https://dekaigi.org/">
+        <div className="flex p-5">
+          <Image
+            src="/images/logo_dekkaigi_black.png"
+            alt="デッカイギロゴ"
+            width={170}
+            height={35}
+          />
+        </div>
+      </Link>
       {process.env.NEXT_PUBLIC_SESSIONIZE_ID ? (
         <SessionizeSessions id={process.env.NEXT_PUBLIC_SESSIONIZE_ID!} />
       ) : (
