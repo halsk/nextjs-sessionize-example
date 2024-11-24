fc8fd3581847d9bebc5fd0e26abc10abe832eb59
Hal Seki <hal@plants-web.jp>
2024-11-24 17:36:21 +0900

    ぼっち飯を準備中に

--- src/components/atoms/BocchimeshiButton.tsx
+++ src/components/atoms/BocchimeshiButton.tsx
@@ -1,15 +1,12 @@
-import Link from "next/link";
 
 export const BocchimeshiButton: React.FC = () => {
   return (
     <>
-      <Link
-        href="https://docs.google.com/spreadsheets/d/1uNThUmu58ZgzBUdQ8o5I4Stpb2z3-tWP-AWQL8tSgDE/edit#gid=11113144"
+      <button
         className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:bg-secondary font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
-        target="_blank"
       >
-        ぼっち飯対策企画🙌
-      </Link>
+        ぼっち飯対策企画（準備中）🙌
+      </button>
     </>
   );
 };
