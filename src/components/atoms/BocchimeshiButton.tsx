c93378f98c17ff98e997294581844b0547b5ee27
Hal Seki <hal@plants-web.jp>
2024-12-17 22:28:12 +0900

    enable bocchi link

--- src/components/atoms/BocchimeshiButton.tsx
+++ src/components/atoms/BocchimeshiButton.tsx
@@ -2,11 +2,12 @@
 export const BocchimeshiButton: React.FC = () => {
   return (
     <>
-      <button
+      <a href="https://docs.google.com/spreadsheets/d/1FdyzZ9aWJ46plRu1jpFUOajZo65yEWGRqjsV1ma_smY/edit?gid=11113144#gid=11113144"
         className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:bg-secondary font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
+        target="_blank"
       >
-        ぼっち飯対策企画（準備中）🙌
-      </button>
+        ぼっち飯対策企画🙌
+      </a>
     </>
   );
 };
